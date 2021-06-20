13884c6c4547314436f4fb35585e2485fe6ae03c
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-06-20 19:48:15 +1200

    Add map barrier and improve block list interface

--- Assets/Scripts/WorldGen.cs
+++ Assets/Scripts/WorldGen.cs
@@ -1,24 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldGen : MonoBehaviour {
 
-	public Transform grass, dirt, stone, bedrock;
+	[Serializable]
+	public struct Block {
+		public string name;
+		public Transform model;
+	}
+	public Block[] blockData;
 	public Transform terrain;
+	public int groundLevel = 12;
+	public int bedrockLevel = 0;
+	public int mapSize = 24;
+	public int mapCorner = 0;
+
+	private Dictionary<string, Transform> blocks = new Dictionary<string, Transform>();
 
 	void Start() {
-		int groundLevel = 12;
-		int mapSize = 24;
-		for (int i = 0; i < mapSize; i++) {
-			for (int j = 0; j < mapSize; j++) {
-				SpawnBlock(grass, i, groundLevel, j);
-				SpawnBlock(dirt, i, groundLevel - 1, j);
-				SpawnBlock(dirt, i, groundLevel - 2, j);
-				for (int y = 3; y < groundLevel; y++) {
-					SpawnBlock(stone, i, groundLevel - y, j);
+		// Assign blocks
+		foreach (Block block in blockData) {
+			blocks.Add(block.name, block.model);
+		}
+
+		// Generate map
+		for (int i = mapCorner - 1; i <= mapSize; i++) {
+			for (int y = bedrockLevel; y <= groundLevel + 10; y++) {
+				SpawnBlock(blocks["barrier"], i, y, mapCorner - 1);
+				SpawnBlock(blocks["barrier"], i, y, mapSize);
+				SpawnBlock(blocks["barrier"], mapCorner - 1, y, i);
+				SpawnBlock(blocks["barrier"], mapSize, y, i);
+			}
+		}
+		for (int x = mapCorner; x < mapSize; x++) {
+			for (int z = mapCorner; z < mapSize; z++) {
+				SpawnBlock(blocks["grass"], x, groundLevel, z);
+				SpawnBlock(blocks["dirt"], x, groundLevel - 1, z);
+				SpawnBlock(blocks["dirt"], x, groundLevel - 2, z);
+				for (int y = bedrockLevel + 3; y < groundLevel; y++) {
+					SpawnBlock(blocks["stone"], x, groundLevel - y, z);
 				}
-				SpawnBlock(bedrock, i, 0, j);
+				SpawnBlock(blocks["bedrock"], x, bedrockLevel, z);
 			}
 		}
     }
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 *Still in very early development*
 
-Blazt Zone is an upcoming destruction game made in Unity where you must destroy a predetermined level with as little material as possible.
+Blast Zone is an upcoming destruction game made in Unity where you must destroy a predetermined level with as little material as possible.
